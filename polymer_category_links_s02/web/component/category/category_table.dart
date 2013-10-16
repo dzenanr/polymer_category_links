16d1a90a789ee3439a26681a1a0f7492839f4542
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-10-16 11:55:19 -0400

    add s02

---
+++ polymer_category_links_s02/web/component/category/category_table.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+import 'package:polymer_category_links/category_links.dart';
+import 'package:polymer/polymer.dart';
+
+@CustomTag('category-table')
+class CategoryTable extends PolymerElement {
+  @published Categories categories;
+  @observable bool showAdd = false;
+
+  show(Event e, var detail, Node target) {
+    ButtonElement addCategory = shadowRoot.query("#show-add");
+    if (addCategory.text == 'Show Add') {
+      showAdd = true;
+      addCategory.text = 'Hide Add';
+    } else {
+      showAdd = false;
+      addCategory.text = 'Show Add';
+    }
+  }
+}
