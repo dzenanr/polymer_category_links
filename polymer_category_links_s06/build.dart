16cb326d056e34b4eab3fda8213a3cdad17047ff
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-03-24 12:09:14 -0400

    initial update to polymer 0.15.5

--- polymer_category_links_s06/build.dart
+++ polymer_category_links_s06/build.dart
@@ -1,5 +1,7 @@
-import 'package:polymer/builder.dart';
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
 
-void main() {
-  build(entryPoints: ['web/polymer_category_links.html']);
-}
+// This file is only used by Dart Editor. It displays errors and warnings after
+// analyzing a polymer.dart app.
+
+export 'package:polymer/default_build.dart';
